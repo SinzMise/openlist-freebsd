1f380d1e906fe39e2b79eca7731f5de373cea25b
王九弦SZ·Ninty <120767492+SinzMise@users.noreply.github.com>
2025-06-15 17:39:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-appName="alist"
+appName="openlist"
 builtAt="$(date +'%F %T %z')"
 goVersion=$(go version | sed 's/go version //')
-gitAuthor="Xhofe <i@nn.ci>"
+gitAuthor=$(git show -s --format='format:%aN <%ae>' HEAD)
 gitCommit=$(git log --pretty=format:"%h" -1)
 version=$(git describe --long --tags --dirty --always)
-webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/alist-org/alist-web/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
-
+webVersion=$(curl -s --max-time 5 "https://api.github.com/repos/OpenListTeam/OpenList-Frontend/releases/latest" -L | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/^v//')
+if [ -z "$webVersion" ]; then
+    webVersion="0.0.0"
+fi
 ldflags="\
 -w -s \
--X 'github.com/alist-org/alist/v3/internal/conf.BuiltAt=$builtAt' \
--X 'github.com/alist-org/alist/v3/internal/conf.GoVersion=$goVersion' \
--X 'github.com/alist-org/alist/v3/internal/conf.GitAuthor=$gitAuthor' \
--X 'github.com/alist-org/alist/v3/internal/conf.GitCommit=$gitCommit' \
--X 'github.com/alist-org/alist/v3/internal/conf.Version=$version' \
--X 'github.com/alist-org/alist/v3/internal/conf.WebVersion=$webVersion' \
+-X 'github.com/OpenListTeam/OpenList/internal/conf.BuiltAt=$builtAt' \
+-X 'github.com/OpenListTeam/OpenList/internal/conf.GoVersion=$goVersion' \
+-X 'github.com/OpenListTeam/OpenList/internal/conf.GitAuthor=$gitAuthor' \
+-X 'github.com/OpenListTeam/OpenList/internal/conf.GitCommit=$gitCommit' \
+-X 'github.com/OpenListTeam/OpenList/internal/conf.Version=$version' \
+-X 'github.com/OpenListTeam/OpenList/internal/conf.WebVersion=$webVersion' \
 "
 
 go build -ldflags="$ldflags" -tags=jsoniter .
